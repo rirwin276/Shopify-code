2b3a624f52dfee971788421f8184aa128761264a
rirwin276 <ryan.irwin@stellaandsagecompany.com>
2026-06-02 23:52:53 -0700

    Add CC1717 price editor row

---
+++ assets/ss-pe-cc1717.js
@@ -0,0 +1,22 @@
+(function(){
+  function row(){
+    var d=document.createElement('div');
+    d.className='ap-price-row';
+    d.setAttribute('data-ss-cc1717','1');
+    d.innerHTML='<div class="ap-price-label">Comfort Colors CC1717 Heavyweight T-Shirt</div><div class="ap-inputs-group"><div class="ap-input-col"><span class="ap-col-label">Front or Back</span><div class="ap-input-wrap"><span class="ap-dollar">$</span><input class="ap-price-input" type="number" min="1" max="999" step="0.01" data-module="CC1717_front" value=""></div></div><div class="ap-input-col"><span class="ap-col-label">Two Images</span><div class="ap-input-wrap"><span class="ap-dollar">$</span><input class="ap-price-input" type="number" min="1" max="999" step="0.01" data-module="CC1717_front_back" value=""></div></div></div>';
+    return d;
+  }
+  function add(){
+    if(location.pathname.indexOf('/pages/price-editor')===-1)return false;
+    if(document.querySelector('[data-ss-cc1717="1"]'))return true;
+    var t=document.getElementById('peTable');
+    if(!t)return false;
+    var m=t.querySelector('input[data-module="LS14003_front_back"]')||t.querySelector('input[data-module="M2580_front_back"]');
+    var r=m&&m.closest?m.closest('.ap-price-row'):null;
+    if(r&&r.parentNode)r.parentNode.insertBefore(row(),r.nextSibling);else t.insertBefore(row(),t.firstChild);
+    return true;
+  }
+  add();
+  var n=0,i=setInterval(function(){n++;if(add()||n>200)clearInterval(i)},20);
+  new MutationObserver(add).observe(document.documentElement,{childList:true,subtree:true});
+})();
